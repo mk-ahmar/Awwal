591f414ca0e045d787460735cd0f66bcb2ad725c
MD KASHF AHMAR <kashf@MDs-MacBook-Air.local>
2025-10-13 23:15:29 +0530

    Added second API controller, using Git in VSCode terminal to push this

--- src/main/java/com/awwal/api/contollers/UserController.java
+++ src/main/java/com/awwal/api/contollers/UserController.java
@@ -3,6 +3,7 @@ package com.awwal.api.contollers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +23,11 @@ public class UserController {
 	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
 		UserDto createUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/search")
+	public ResponseEntity<UserDto> searchUser(@RequestBody int userId) {
+		UserDto searchUserDto = this.userService.getUserById(userId);
+		return new ResponseEntity<>(searchUserDto, HttpStatus.FOUND);
 	}
 }
