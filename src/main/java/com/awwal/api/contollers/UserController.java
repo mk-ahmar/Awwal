cd15e3fd12dc4ab672696707ec7b2eb9c78f6832
MD KASHF AHMAR <kashf@MDs-MacBook-Air.local>
2025-12-06 12:19:28 +0530

    chore: commit local changes

--- src/main/java/com/awwal/api/contollers/UserController.java
+++ src/main/java/com/awwal/api/contollers/UserController.java
@@ -1,9 +1,13 @@
 package com.awwal.api.contollers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity;			
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;					
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,19 +19,31 @@ import com.awwal.api.services.UserService;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@PostMapping("/")
 	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
 		UserDto createUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
 	}
 
-	@GetMapping("/search")
-	public ResponseEntity<UserDto> searchUser(@RequestBody int userId) {
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> searchUser(@PathVariable int userId) {
 		UserDto searchUserDto = this.userService.getUserById(userId);
-		return new ResponseEntity<>(searchUserDto, HttpStatus.FOUND);
+		return new ResponseEntity<>(searchUserDto, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<UserDto> deleteUser(@PathVariable int userId) {
+		userService.deleteUser(userId);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUser() {
+		List<UserDto> users = this.userService.getAllUsers();
+		return new ResponseEntity<>(users, HttpStatus.OK);
 	}
 }
